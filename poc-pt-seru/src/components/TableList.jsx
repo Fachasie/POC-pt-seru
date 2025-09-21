79893bf94edfbfa7737673f7cdcbff6c75c4e92f
Fachasie <leonardofachasie3@gmail.com>
2025-09-22 01:08:33 +0700

    Rapih Rapih Sedikit

--- poc-pt-seru/src/components/TableList.jsx
+++ poc-pt-seru/src/components/TableList.jsx
@@ -142,7 +142,7 @@ export default function TableList() {
 
 
   return (
-    <div className="p-4 bg-gray-100 min-h-screen">
+    <div className="p-4 bg-gray-200 min-h-screen">
       {/* Modal for Alerts and Confirmations */}
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
